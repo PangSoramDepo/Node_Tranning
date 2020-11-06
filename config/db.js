e4307b45a6afee0afc07a806fbbe5ffe5aaaa72f
pangsoramdepo <pangsoramdepo@gmail.com>
2020-11-06 16:29:31 +0700

    Initialize with Api Mongo

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : "mongodb://depo:depo1234@cluster0-shard-00-00.pjzgp.mongodb.net:27017,cluster0-shard-00-01.pjzgp.mongodb.net:27017,cluster0-shard-00-02.pjzgp.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-rxnxlz-shard-0&authSource=admin&retryWrites=true&w=majority"
+  };
