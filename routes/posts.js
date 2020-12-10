6b971a500153d111cc7e5a939f629f60052aa53b
PANGSORAMDEPO <pangsoramdepo@gmail.com>
2020-12-10 22:31:17 +0700

    Finish Crud

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 
 const Posts = new mongoose.model('Post',new mongoose.Schema({
     title: {
@@ -9,15 +10,31 @@ const Posts = new mongoose.model('Post',new mongoose.Schema({
         minlength: 5,
         maxlength: 20
     },
+    category: {
+        type: String,
+        enum: ["Web","Mobile","Network"],
+        lowercase: true,
+        // uppercase: true,
+        trim: true
+    },
     body: String,
 }));
 
-router.get('/',async(req,res)=>{
+router.get('/',async (req,res)=>{
     const posts = await Posts.find().sort('-title');
     res.send(posts);
 });
 
+router.get('/:id',async (req,res)=>{
+    const posts = await Posts.findById(req.params.id);
+    if (!posts) return res.status(404).send(`The post with the given ID ${req.params.id} not found`);
+    res.send(posts);
+});
+
 router.post('/',async (req,res)=>{
+    const {error} = validatePost(req.body); // error = result.error (Object Destructuring)
+    if(error) return res.status(400).send(error.details[0].message);
+
     let post = new Posts({
         title: req.body.title,
         body: req.body.body
@@ -27,14 +44,29 @@ router.post('/',async (req,res)=>{
 });
 
 router.put('/:id',async (req,res)=>{
+    const {error} = validatePost(req.body); // error = result.error (Object Destructuring)
+    if(error) return res.status(400).send(error.details[0].message);
+
     let post = await Posts.findByIdAndUpdate(req.params.id,{
         title: req.body.title,
         body: req.body.body
-    },{ 
-        new: true
-    });
+    },{ new: true });
     if(!post) return res.status(404).send("The post with the given ID not found...");
     res.send(post);
 });
+
+router.delete('/:id',async (req,res)=>{
+    let post = await Posts.findByIdAndRemove(req.params.id);
+    if (!post) return res.status(404).send(`The post with the given ID ${req.params.id} not found`);
+    res.send(post); 
+});
+
+function validatePost(posts){
+    const schema = {
+        title : Joi.string().min(3).required(),
+        body : Joi.allow()
+    };
+    return Joi.validate(posts,schema);
+}
 
 module.exports = router;
