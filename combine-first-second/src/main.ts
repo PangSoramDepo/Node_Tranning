75fc7fdad4e6172699b0068aaf7a8c96adac4c4b
PANGSORAMDEPO <pangsoramdepo@gmail.com>
2020-12-16 23:59:04 +0700

    Add Frontend - EnableCORS orign with AWS S3

--- combine-first-second/src/main.ts
+++ combine-first-second/src/main.ts
@@ -9,6 +9,10 @@ async function bootstrap() {
   const port = process.env.PORT || serverConfig.port;
 
   const app = await NestFactory.create(AppModule);
+  if(process.env.NODE_ENV === 'development')
+    app.enableCors();
+  else
+    app.enableCors({ origin: serverConfig.origin });
   await app.listen(port);
   logger.log(`Application listen on port ${port}`);
 }
