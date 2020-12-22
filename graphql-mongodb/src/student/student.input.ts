24119cc29d5cf0024e9ea939b92ba6b812ac2c55
PANGSORAMDEPO <pangsoramdepo@gmail.com>
2020-12-22 22:40:00 +0700

    Add Student module v-12-19 difficult one

---
+++ graphql-mongodb/src/student/student.input.ts
@@ -0,0 +1,11 @@
+import { Field, InputType } from "@nestjs/graphql";
+
+@InputType()
+export class CreateStudentInput{
+
+    @Field()
+    firstName: string;
+
+    @Field()
+    lastName: string;
+}
